704b1854b3ccbf044c3a5597ea5cd335d020c923
John Atkins <jdatkins@tql.com>
2021-05-19 16:35:23 -0400

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tqlmath = new TqlMath();
+            tqlmath.A = 21;
+            tqlmath.B = 30;
+            var sum = tqlmath.Sum();
+            Console.WriteLine($"Sum of {tqlmath.A} and {tqlmath.B} is {sum}.");
+
+            var diff = tqlmath.Diff();
+            Console.WriteLine($"Diff of {tqlmath.A} and {tqlmath.B} is {diff}.");
+
+        }
+    }
+}
